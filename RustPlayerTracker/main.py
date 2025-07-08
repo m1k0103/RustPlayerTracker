a93433462360a4274d7f97dfda7acd6cc35886f5
miko <84585119+m1k0103@users.noreply.github.com>
2025-07-08 01:15:21 +0100

    Implemented telegram notifications

--- RustPlayerTracker/main.py
+++ RustPlayerTracker/main.py
@@ -1,9 +1,5 @@
 from RustPlayerTracker.func import *
-from bs4 import BeautifulSoup 
-import requests
 import time
-import re
-import yaml
 import os
 
 # sets the players that will be monitored
@@ -18,14 +14,21 @@ def main():
         # finds the names of all players using a regex pattern on all anchor tags
         allPlayerNames = getOnlinePlayers()
 
-        #goes through each player in the list and checks if they are online
+        telegramMessageText = ""
+        # Goes through each player in the list and checks if they are online
+        # and then builds a message to be sent to the telegram webhook.
         for player in playersToTrack:
             if player in allPlayerNames:
                 print(f"{player} is still in game.")
+                telegramMessageText += f"{player} is in game.\n"
                 setOnlineInConfig(player,1)
             else:
                 print(f"{player} IS OFFLINE")
+                telegramMessageText += f"{player} IS OFFLINE\n"
                 setOnlineInConfig(player,0)
 
         print(f"Currently online: {getTotalOnlineFromConfig()}/{len(getPlayersToWatch())}")
+        telegramMessageText += f"\nCurrently online: {getTotalOnlineFromConfig()}/{len(getPlayersToWatch())}"
+        sendTelegramNotification(telegramMessageText)
+        
         time.sleep(60)
