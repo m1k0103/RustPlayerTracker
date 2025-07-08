f588f3b47ba7598cd3d5e675ebaec1e60efc6382
miko <84585119+m1k0103@users.noreply.github.com>
2025-07-08 01:15:06 +0100

    Changed YAML library
    Now the YAML library preserves comments and stuff because it reads it as python objects. So epic.

--- RustPlayerTracker/func.py
+++ RustPlayerTracker/func.py
@@ -1,7 +1,17 @@
-import yaml
+#import yaml
 import requests
 from bs4 import BeautifulSoup
 import re
+from ruamel.yaml import YAML
+
+# Sets the YAML object to be global, so it doesnt need to be redeclared with each function that uses it.
+# Might be a bit inefficient but oh well, it works and its 1am.
+global yaml
+yaml = YAML()
+yaml.preserve_quotes = True  # Optional, keeps quotes
+yaml.indent(mapping=2, sequence=4, offset=2)
+
+
 
 # Fetches the currently online players and returns an array
 def getOnlinePlayers():
@@ -16,31 +26,30 @@ def getOnlinePlayers():
 # Returns the player names that are in the config
 def getPlayersToWatch():
     with open("config.yml", "r", encoding="utf-8") as yml:
-        data = yaml.safe_load(yml)
+        data = yaml.load(yml)
         players = [i[0] for i in data["players"]]
         return players
 
 # Returns the battlemetrics url saved in the config
 def getBattlemetricsUrl():
     with open("config.yml", "r", encoding="utf-8") as yml:
-        url = yaml.safe_load(yml)["battlemetricsUrl"]
+        url = yaml.load(yml)["battlemetricsUrl"]
     return url
 
 # Toggles if a user is shown as online or offline in the config 
 def setOnlineInConfig(playerName, value):
     # Loads current config
     with open("config.yml", "r", encoding="utf-8") as yml:
-        raw_data = yaml.safe_load(yml)
+        raw_data = yaml.load(yml)
     # Finds the player in config, then sets their online status to the set value
     for p in raw_data["players"]:
         if p[0] == playerName:
             p[1] = value
-        else:
-            print(f"{playerName} not found in config.")
+            break
             
     # Dumps the newly edited config
     with open("config.yml", "w", encoding="utf-8") as newYml:
-        yaml.safe_dump(newYml)    
+        yaml.dump(raw_data, newYml)    
     return
     
 
@@ -48,7 +57,7 @@ def setOnlineInConfig(playerName, value):
 def getTotalOnlineFromConfig():
     # Opens config, loads into variable
     with open("config.yml", "r", encoding="utf-8") as yml:
-        data = yaml.safe_load(yml)
+        data = yaml.load(yml)
     # Goes through each player and adds 1 to the final count
     count = 0
     for p in data["players"]:
@@ -68,13 +77,20 @@ def checkIfPlayerOnServer(username):
 # Gets the bot token and channel ID used for sending messages to Telegram from the config file.
 def getTGConfig():
     with open("config.yml") as cfg:
-        contents = yaml.safe_load(cfg)
+        contents = yaml.load(cfg)
+
+    # If the values in the config are empty
+    if (contents["tgBotToken"] == "") and (contents["tgChannelID"] == ""):
+        return False
+
     return [contents["tgBotToken"], contents["tgChannelID"]]
 
 
 # Sends message to a telegram channel
 def sendTelegramNotification(message):
     creds = getTGConfig() # index0 = token, index1 = channelID
+    if creds == False: # this means there are no tg creds, so message isnt sent.
+        return
     data = {"chat_id":creds[1], "text":{message}}
     r = requests.post(f"https://api.telegram.org/bot{creds[0]}/sendMessage", data=data)
     if r.status_code == 200:
